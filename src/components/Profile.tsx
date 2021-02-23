dbb483824beff1fba428c66d151c6eb969ff7d9e
RichardFelipe-collab <richardfelipe.123@hotmail.com>
2021-02-23 18:22:44 -0300

    implements components countdowns and styles

---
+++ src/components/Profile.tsx
@@ -0,0 +1,17 @@
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/RichardFelipe-collab.png" alt="" />
+      <div>
+        <strong>Richard Felipe</strong>
+
+        <p>
+          <img src="icons/level.svg" />
+          Level 1
+          </p>
+      </div>
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
+import { CompletedChallenges } from '../components/CompleteChallenges';
+import { Countdown } from '../components/countDown';
 import { ExperienceBar } from '../components/ExperienceBar';
+import { Profile } from '../components/Profile';
+import styles from '../styles/pages/Home.module.css';
+
+import Head from 'next/head';
 
 function App() {
   return (
-    <div className="container">
+    <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
       <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+        <div>
+
+        </div>
+      </section>
     </div>
   );
 }
